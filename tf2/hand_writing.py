3ebda7416563e7a062c9f5749526bd5cae01a7db
JeongMyeongHong <hkdhjm@gmail.com>
2022-06-28 15:54:17 +0900

    2022-06-28 functional api

--- tf2/hand_writing.py
+++ tf2/hand_writing.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow import keras
 from icecream import ic
 
@@ -8,22 +7,39 @@ class Solution:
     def __init__(self):
         self.mnist = keras.datasets.mnist
 
-        (train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
-        self.train_images = train_images.reshape((60000, 28, 28, 1))
+        (train_images, self.train_labels), (test_images, self.test_labels) = keras.datasets.mnist.load_data()
+        train_images = train_images.reshape((60000, 28, 28, 1))
         test_images = test_images.reshape((10000, 28, 28, 1))
 
         # 픽셀 값을 0~1 사이로 정규화합니다.
-        train_images, test_images = train_images / 255.0, test_images / 255.0
+        self.train_images, self.test_images = train_images / 255.0, test_images / 255.0
+        self.model = keras.models.Sequential()
 
     def solution(self):
-        model = keras.models.Sequential()
+        self.modeling()
+        self.training()
+
+    def modeling(self):
+        model = self.model
         model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
         model.add(keras.layers.MaxPooling2D((2, 2)))
         model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(keras.layers.MaxPooling2D((2, 2)))
         model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(keras.layers.Flatten())
+        model.add(keras.layers.Dense(64, activation='relu'))
+        model.add(keras.layers.Dense(10, activation='softmax'))
+        model.summary()
+
+    def training(self):
+        model = self.model
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.fit(self.train_images, self.train_labels, epochs=5)
 
 
 if __name__ == '__main__':
-    ic(np.shape(Solution().train_images))
+    ic(np.shape(Solution().solution()))
 
